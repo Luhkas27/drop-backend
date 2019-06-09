251afecd37ccba0241b75ff839fbcfa5adc84b1b
Lucas Cardoso <lucas.cardoso@mblabs.com.br>
2019-06-09 16:20:23 -0300

    Estrutura Backend

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+app.io(cors());
+
+const server = require("http").Server(app);
+const io = require("socket.io")(Server);
+
+io.on("connection", socket => {
+  socket.on("connectRoom", box => {
+    socket.join(box);
+  });
+});
+
+mongoose.connect("mongodb+srv://omniStack:omniStack@cluster0-tvzbg.mongodb.net/omniStack?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
+
+app.use(require("./routes"));
+
+app.listen(3333);
